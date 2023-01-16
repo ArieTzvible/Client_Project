1c46a218715d4952698959d0817c1279c417091d
Arie <a0548463928@gmail.com>
2023-01-16 17:41:20 +0200

    Project upgrade - sorting of AVL trees

---
+++ Client_Project/HeaderTest.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#ifndef HeaderTest
+#define HeaderTest
+
+#include "HeaderUtilities.h"
+#include "HeaderDelet.h"
+
+void testingTheNewCell(ListManager manager, PClient* newCell);// Checking whether the ID already exists
+void cellUpdateInLitst(PClient* newCell, PClient* temp);//Cell update
+int comparisonOfCells(PClient* newCell, PClient* temp);//Cell comparison
+int comparingDates(Date nweCell, Date temp);//Comparing dates
+void isTheDataCorrect(PClient* client);
+
+#endif
